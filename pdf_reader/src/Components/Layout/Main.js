38b3966d971183b823d548562c8bb99fe5890f96
THT <thehelpfultipper@gmail.com>
2023-12-15 20:30:36 -0800

    Fix class render on start button

--- pdf_reader/src/Components/Layout/Main.js
+++ pdf_reader/src/Components/Layout/Main.js
@@ -8,7 +8,7 @@ import s from './Main.module.scss';
 function Main({start, onStart}) {
     return (
         <main className={s.wrapper}>
-            <button type="button" onClick={onStart} className={start ? s.reset : ''}>{start ? 'Reset' : 'Click to start'}</button>
+            <button type="button" onClick={onStart} className={start ? s.reset : null}>{start ? 'Reset' : 'Click to start'}</button>
             {
                 // Upload file component
                 start && <UploadProvider><SearchForm /></UploadProvider>
